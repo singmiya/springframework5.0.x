5dbc141ec97b305536e503a911c8519b3541b679
singmiya <jjsomiya@gmail.com>
2020-06-17 19:44:48 +0800

    <singsmiya：jjsomiya@gmail.com> 注释： spring-beans/src/main/java/org/springframework/beans/factory/support/BeanDefinitionRegistry.java spring-core/src/main/java/org/springframework/core/AliasRegistry.java

--- spring-core/src/main/java/org/springframework/core/AliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/AliasRegistry.java
@@ -20,6 +20,8 @@ package org.springframework.core;
  * Common interface for managing aliases. Serves as super-interface for
  * {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}.
  *
+ * 用于管理别名的公共接口。用作BeanDefinitionRegistry的超级接口(super-interface).
+ *
  * @author Juergen Hoeller
  * @since 2.5.2
  */
@@ -27,15 +29,21 @@ public interface AliasRegistry {
 
 	/**
 	 * Given a name, register an alias for it.
-	 * @param name the canonical name
-	 * @param alias the alias to be registered
+	 *
+	 * 为给定名称注册一个别名。
+	 *
+	 * @param name the canonical name 规范名称
+	 * @param alias the alias to be registered 要注册的别名
 	 * @throws IllegalStateException if the alias is already in use
-	 * and may not be overridden
+	 * and may not be overridden 如果别名正在使用，则不能将其覆盖
 	 */
 	void registerAlias(String name, String alias);
 
 	/**
 	 * Remove the specified alias from this registry.
+	 *
+	 * 把指定的别名从此注册表中移除。
+	 *
 	 * @param alias the alias to remove
 	 * @throws IllegalStateException if no such alias was found
 	 */
@@ -44,6 +52,9 @@ public interface AliasRegistry {
 	/**
 	 * Determine whether this given name is defines as an alias
 	 * (as opposed to the name of an actually registered component).
+	 *
+	 * 确定此名称是否是定义的别名（相对于实际注册的组件名称）。
+	 *
 	 * @param name the name to check
 	 * @return whether the given name is an alias
 	 */
@@ -51,6 +62,9 @@ public interface AliasRegistry {
 
 	/**
 	 * Return the aliases for the given name, if defined.
+	 *
+	 * 如果已定义过，则返回给定名称（所对应的）的别名。
+	 *
 	 * @param name the name to check for aliases
 	 * @return the aliases, or an empty array if none
 	 */
